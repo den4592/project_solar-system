f6526d50569b8d954b26affff002c83b6d51c582
ilya <den4592@naver.com>
2022-05-28 01:27:58 +0900

    set gravity value

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector("button");
 const inputValue = document.querySelector("input");
 const planet = document.querySelector("#planet");
+const SURFACE_GRAVITY = 9.80665;
 let result = 0;
 
 function setValue(result, a) {
@@ -21,31 +22,31 @@ function setValue(result, a) {
 
 btn.addEventListener("click", (e) => {
   if (planet.value == "earth") {
-    result = parseInt(inputValue.value * 1.0 * 9.80665);
+    result = parseInt(inputValue.value * 1.0 * SURFACE_GRAVITY);
     setValue(result, "earth");
   } else if (planet.value == "mars") {
-    result = parseInt(inputValue.value * 0.38 * 9.80665);
+    result = parseInt(inputValue.value * 0.38 * SURFACE_GRAVITY);
     setValue(result, "mars");
   } else if (planet.value == "mercury") {
-    result = parseInt(inputValue.value * 0.38 * 9.80665);
+    result = parseInt(inputValue.value * 0.38 * SURFACE_GRAVITY);
     setValue(result, "mercury");
   } else if (planet.value == "venus") {
-    result = parseInt(inputValue.value * 0.91 * 9.80665);
+    result = parseInt(inputValue.value * 0.91 * SURFACE_GRAVITY);
     setValue(result, "venus");
   } else if (planet.value == "moon") {
-    result = parseInt(inputValue.value * 1.62 * 9.80665);
+    result = parseInt(inputValue.value * 1.62 * SURFACE_GRAVITY);
     setValue(result, "moon");
   } else if (planet.value == "jupiter") {
-    result = parseInt(inputValue.value * 2.34 * 9.80665);
+    result = parseInt(inputValue.value * 2.34 * SURFACE_GRAVITY);
     setValue(result, "jupiter");
   } else if (planet.value == "saturn") {
-    result = parseInt(inputValue.value * 0.93 * 9.80665);
+    result = parseInt(inputValue.value * 0.93 * SURFACE_GRAVITY);
     setValue(result, "saturn");
   } else if (planet.value == "uranus") {
-    result = parseInt(inputValue.value * 0.92 * 9.80665);
+    result = parseInt(inputValue.value * 0.92 * SURFACE_GRAVITY);
     setValue(result, "uranus");
   } else if (planet.value == "neptune") {
-    result = parseInt(inputValue.value * 1.12 * 9.80665);
+    result = parseInt(inputValue.value * 1.12 * SURFACE_GRAVITY);
     setValue(result, "neptune");
   }
 });
